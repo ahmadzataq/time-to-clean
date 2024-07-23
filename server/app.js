26247a39bccb8c16671f0dbc840524f56574e29f
Miluvjay <zahidtaqiyuddinahmad@gmail.com>
2024-07-23 12:11:48 +0700

    update

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: 'https://timetoclean.vercel.app/',
+  origin: 'https://timetoclean.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS', 'HEAD'], 
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin'], 
 };
