cc4698249dfe56c28006d2ecf10e1476ef088136
Miluvjay <zahidtaqiyuddinahmad@gmail.com>
2024-07-25 22:33:12 +0700

    update cartitems.jsx

--- frontend/admin/src/component/dashboard/CartItems.jsx
+++ frontend/admin/src/component/dashboard/CartItems.jsx
@@ -3,20 +3,31 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const CartItems = () => {
-  // Orders
   const [orders, setOrders] = useState([]);
   const [completeOrders, setCompleteOrders] = useState([]);
+
   useEffect(() => {
-    const fatchOrders = async () => {
-      const { data } = await axios.get("https://time-to-clean-api.vercel.app/orders");
-      const completeOrder = data.filter((curData) => {
-        return curData.status.toLowerCase() === "selesai";
-      });
-      setCompleteOrders(completeOrder);
-      setOrders(data);
+    const fetchOrders = async () => {
+      try {
+        const { data } = await axios.get("https://time-to-clean-api.vercel.app/orders");
+
+        // Cek apakah data adalah array
+        if (Array.isArray(data)) {
+          const completeOrder = data.filter((curData) => {
+            return curData.status.toLowerCase() === "selesai";
+          });
+          setCompleteOrders(completeOrder);
+          setOrders(data);
+        } else {
+          console.error("Data yang diterima bukan array:", data);
+        }
+      } catch (error) {
+        console.error("Error fetching orders:", error);
+      }
     };
-    fatchOrders();
-  }, [orders]);
+
+    fetchOrders();
+  }, []); // Perhatikan bahwa dependensi di sini harus [] untuk menghindari loop tak terbatas
 
   return (
     <>
@@ -28,7 +39,7 @@ const CartItems = () => {
               <span>{orders.length}+</span>
             </div>
             <span className="card-icon">
-              <i class="ri-shopping-basket-line"></i>
+              <i className="ri-shopping-basket-line"></i>
             </span>
           </div>
         </Link>
@@ -40,14 +51,13 @@ const CartItems = () => {
               <span>{completeOrders.length}+</span>
             </div>
             <span className="card-icon">
-              <i class="ri-check-double-fill"></i>
+              <i className="ri-check-double-fill"></i>
             </span>
           </div>
         </Link>
-
       </div>
       <div className="dashboard-cards">
-    </div>
+      </div>
     </>
   );
 };
