1f4024ce7387e22dcb8a36cb8d9b5e726d15d933
Ahmad Zahid <153094713+ahmadzataq@users.noreply.github.com>
2024-07-26 21:17:46 +0700

    Update setupProxy.js

--- users/src/setupProxy.js
+++ users/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/router/auth',
+    '/api/admin',
     createProxyMiddleware({
       target: 'https://time-to-clean-api.up.railway.app',
       changeOrigin: true,
