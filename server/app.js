98c1e0beb44f237647c3fbcf5f3ea93e850fc35e
gochujjang <41176663+gochujjang@users.noreply.github.com>
2024-07-30 15:44:46 +0700

    fix: cors

--- server/app.js
+++ server/app.js
@@ -27,7 +27,7 @@ app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-app.options('*', cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
